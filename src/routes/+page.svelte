6222db51b9c873a59977e4c8a641ec9a05bb7b37
dawitet <dawitfikadu3@gmail.com>
2025-07-08 21:17:51 +0300

    Fix: Prevent SSR for GameScreen component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import GameScreen from '$lib/components/GameScreen.svelte';
+
+  let showGame = false;
+
+  onMount(() => {
+    // This code only runs in the browser, not on the server.
+    showGame = true;
+  });
 </script>
 
-<GameScreen />
+{#if showGame}
+  <GameScreen />
+{/if}
