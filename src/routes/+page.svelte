57cf024899e5bc0c26bdd947b00bf5647bc79109
dawitet <dawitfikadu3@gmail.com>
2025-07-08 21:23:18 +0300

    Fix: Disable SSR for the game page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
-  let GameScreen: any;
-
-  onMount(async () => {
-    // Dynamically import the component only on the client-side.
-    const module = await import('$lib/components/GameScreen.svelte');
-    GameScreen = module.default;
-  });
+  import GameScreen from '$lib/components/GameScreen.svelte';
 </script>
 
-{#if GameScreen}
-  <svelte:component this={GameScreen} />
-{:else}
-  <p class="text-center mt-10">Loading game...</p>
-{/if}
+<GameScreen />
